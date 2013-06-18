432cc6a54893718173231f434e6e124c09faa228
Mihir Parikh <mihir1408@gmail.com>
2013-06-18 19:16:30 +1000

    PHP XML DOM processing

--- dom/quotes/quotes.php
+++ dom/quotes/quotes.php
@@ -1,6 +1,18 @@
 <?php
 $doc = new DOMDocument;
-$doc->load('quote.xml');
-$strTarget = $argv[1];
-// your code goes here
+$doc->load('quotes.xml');
+//Passed element from commandline
+$passedString = $argv[1];
+
+$passedElements = $doc->getElementsByTagName($passedString);
+
+foreach($passedElements as $passedElement){
+   $parent = $passedElement->parentNode;
+   
+   while($parent->nodeType != XML_DOCUMENT_NODE){
+      echo $parent->tagName." ";
+      $parent = $parent->parentNode;
+   }   
+}
+
 ?>
